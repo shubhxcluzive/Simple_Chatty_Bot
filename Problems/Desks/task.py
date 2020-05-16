b5d0f790c25030edc0759a60b2669b7dc9ce1541
shubhxcluzive <shubhxcluzive@gmail.com>
2020-05-16 22:22:12 +0530

    update

--- Problems/Desks/task.py
+++ Problems/Desks/task.py
@@ -1,10 +1,1 @@
 # put your python code here
-no_of_desk = 0
-n1 = int(input())
-n2 = int(input())
-n3 = int(input())
-if (n1 + n2 + n3)%2 == 0:
-    no_of_desk = (n1 + n2 + n3)/2
-else:
-    no_of_desk = int((n1 + n2 + n3)/2) + 1
-print(int(no_of_desk))
---
+++ Problems/Very odd/task.py
@@ -0,0 +1,6 @@
+# put your python code here
+dividend = int(input())
+divider = int(input())
+ans = dividend // divider
+
+print(ans % 2 == 1)
