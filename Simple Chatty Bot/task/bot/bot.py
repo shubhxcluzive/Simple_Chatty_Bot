07264701465167d05fccce26aef96035da0bcb2d
shubhxcluzive <shubhxcluzive@gmail.com>
2020-05-18 13:09:43 +0530

    initial bot phase

--- Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/task/bot/bot.py
@@ -1,29 +1,60 @@
-print('Hello! My name is Aid.')
-print('I was created in 2020.')
-print('Please, remind me your name.')
-
-name = input()
-
-print('What a great name you have, ' + name + '!')
-print('Let me guess your age.')
-print('Enter remainders of dividing your age by 3, 5 and 7.')
-
-rem3 = int(input())
-rem5 = int(input())
-rem7 = int(input())
-
-age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
-
-print("Your age is " + str(age) + "; that's a good time to start programming!")
-print('Now I will prove to you that I can count to any number you want.')
-
-# read a number and count to it here
-n = int(input())
-i = 0
-while i < 21:
+def greet(bot_name, birth_year):
+    print('Hello! My name is ' + bot_name + '.')
+    print('I was created in ' + birth_year + '.')
 
 
+def remind_name():
+    print('Please, remind me your name.')
+    name = input()
+    print('What a great name you have, ' + name + '!')
 
-    print(str(i) + " !")
-    i += 1
-print('Completed, have a nice day!')
+
+def guess_age():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+    rem3 = int(input())
+    rem5 = int(input())
+    rem7 = int(input())
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+    print("Your age is " + str(age) + "; that's a good time to start programming!")
+
+
+def count():
+    print('Now I will prove to you that I can count to any number you want.')
+
+    num = int(input())
+    curr = 0
+    while curr <= num:
+        print(curr, '!')
+        curr = curr + 1
+
+
+def test():
+    print("Let's test your programming knowledge.")
+    print("why do we use meth0ds")
+    a = "1. To repeat a statement multiple times."
+    b = "2. To decompose a program into several small subroutines."
+    c = "3. To determine the execution time of a program."
+    d = "4. To interrupt the execution of a program."
+    user_input = int(input())
+
+    if user_input == b:
+        pass
+    else:
+        print("Please, try again")
+
+    print('Completed, have a nice day!')
+
+
+def end():
+    print('Congratulations, have a nice day!')
+
+
+greet('Aid', '2020')  # change it as you need
+remind_name()
+guess_age()
+count()
+test()
+end()
